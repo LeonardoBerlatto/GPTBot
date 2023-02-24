ec1cad27f0712d7e88ecd0ac5e0953214991e5b4
leonardo.berlatto <lberlatto@tempo.io>
2023-02-24 01:04:36 -0300

    ref(discord-api-integration): create constants

--- src/main/kotlin/com/discord/bot/gptbot/service/OpenAiService.kt
+++ src/main/kotlin/com/discord/bot/gptbot/service/OpenAiService.kt
@@ -2,11 +2,13 @@ package com.discord.bot.gptbot.service
 
 import com.discord.bot.gptbot.repository.ChatGptRepository
 
+private const val INVALID_QUESTION_MESSAGE = "Provide a valid question"
+
 class OpenAiService(private val repository: ChatGptRepository) {
 
     fun ask(question: String?): String {
         if (question.isNullOrBlank()) {
-            return "Provide a valid question"
+            return INVALID_QUESTION_MESSAGE
         }
 
         return repository.processText(question)

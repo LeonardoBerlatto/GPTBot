bde66f588aa99a5d546d9767934e71cebc206817
leonardo.berlatto <lberlatto@tempo.io>
2023-02-25 15:35:30 -0300

    chore(chat-gpt-repository): add spring webflux to build reactive request

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     // discord

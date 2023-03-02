641fa50f2eddb6665b0b76c34f8c94167d33e851
leonardo.berlatto <lberlatto@tempo.io>
2023-03-02 11:38:19 -0300

    feat(release-pipeline): create release pipeline

---
+++ actions/get_version.py
@@ -0,0 +1,9 @@
+import os
+
+commit_text = os.environ.get('COMMIT_TEXT', '')
+
+commit_parts = commit_text.split('\n\n')
+
+commit_title = commit_parts[0]
+
+print(commit_title.split(' ')[1])
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.discord.bot"
-version = "0.0.1-SNAPSHOT"
+version = project.properties["version"] as String
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {

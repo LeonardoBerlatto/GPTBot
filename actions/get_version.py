3f8abbc545fafa158987ee55fce271bd8a6080e1
leonardo.berlatto <lberlatto@tempo.io>
2023-03-04 19:32:22 -0300

    release 1.0.0

--- actions/get_version.py
+++ actions/get_version.py
@@ -2,12 +2,8 @@ import os
 
 commit_text = os.environ.get('COMMIT_TEXT', '')
 
-print("Commit text"+commit_text)
-
 commit_parts = commit_text.split('\n\n')
 
 commit_title = commit_parts[0]
-
-print("Version "+ commit_title.split(' ')[1])
 
 print(commit_title.split(' ')[1])

af636ab98ea4474ac427a61b2cb3ea904a1e1336
leonardo.berlatto <lberlatto@tempo.io>
2023-03-04 20:37:56 -0300

    release 1.0.0

--- actions/create_release_notes.py
+++ actions/create_release_notes.py
@@ -4,13 +4,12 @@ commmit_text = os.environ.get('COMMIT_TEXT', '')
 
 commit_parts = commmit_text.split('\n\n')
 
-commit_title = commit_parts[0]
+version = os.environ.get('VERSION', '')
 
 commit_body_lines = commit_parts[1].split('\n')
 
-
 release_notes_file = open('release_notes.txt', 'w')
 
-release_notes_file.write(commit_title)
+release_notes_file.write('Version '.concat(version).concat('\n\n'))
 for line in commit_body_lines:
-    release_notes_file.write('* '.concat(line))
+    release_notes_file.write('* '.concat(line).concat('\n'))

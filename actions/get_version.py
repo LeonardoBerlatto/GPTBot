343c4ee56a484550137dbf4cd495f87d85307b06
leonardo.berlatto <lberlatto@tempo.io>
2023-03-04 22:36:58 -0300

    release 1.0.1 Fix details on the release pipeline Remove dead steps from pipeline

--- actions/get_version.py
+++ actions/get_version.py
@@ -1,22 +1,9 @@
 import os
 
-commmit_text = os.environ.get('COMMIT_TEXT', '')
+commit_text = os.environ.get('COMMIT_TEXT', '')
 
-commit_parts = commmit_text.split('\n')
+commit_parts = commit_text.split('\n')
 
-version = os.environ.get('VERSION', '')
+commit_title = commit_parts[0]
 
-commit_body_lines = commit_parts[1::]
-
-release_notes_file = open('release_notes.txt', 'w')
-
-release_notes_file.write(f'# Version {version}\n\n')
-release_notes_file.write('## Features:\n\n')
-for line in commit_body_lines:
-    release_notes_file.write(f'* {line}\n')
-
-image_sha = os.environ.get('IMAGE_SHA', '')
-docker_username = os.environ.get('DOCKER_USERNAME', '')
-
-release_notes_file.write('## Image:\n\n')
-release_notes_file.write(f'[{version}](https://hub.docker.com/layers/{docker_username}/gpt-bot/{version}/images/sha256-{image_sha})')
+print(commit_title.split(' ')[1])

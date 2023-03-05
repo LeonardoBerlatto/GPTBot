fed543432d02689838d9495335a1269cd9d8fcc5
leonardo.berlatto <lberlatto@tempo.io>
2023-03-04 22:08:41 -0300

    feat(release-pipeline): add docker image to release notes

--- actions/create_release_notes.py
+++ actions/create_release_notes.py
@@ -2,15 +2,21 @@ import os
 
 commmit_text = os.environ.get('COMMIT_TEXT', '')
 
-commit_parts = commmit_text.split('\n\n')
+commit_parts = commmit_text.split('\n')
 
-commit_title = commit_parts[0]
+version = os.environ.get('VERSION', '')
 
-commit_body_lines = commit_parts[1].split('\n')
-
+commit_body_lines = commit_parts[1::]
 
 release_notes_file = open('release_notes.txt', 'w')
 
-release_notes_file.write(commit_title)
+release_notes_file.write(f'# Version {version}\n\n')
+release_notes_file.write('## Features:\n\n')
 for line in commit_body_lines:
-    release_notes_file.write('* '.concat(line))
+    release_notes_file.write(f'* {line}\n')
+
+image_sha = os.environ.get('IMAGE_SHA', '')
+docker_username = os.environ.get('DOCKER_USERNAME', '')
+
+release_notes_file.write('## Image:\n\n')
+release_notes_file.write(f'({version})[https://hub.docker.com/layers/{docker_username}/gpt-bot/{version}/images/sha256-{image_sha}]')
